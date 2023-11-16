a4add3afaac6755b7314c224c7aafb896eccfa40
Tuan PHAM <phamtuantkt@gmail.com>
2023-11-16 14:14:29 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -184,7 +184,7 @@
       mm = String(today.getMonth() + 1).padStart(2, "0"),
       yyyy = today.getFullYear(),
       nextYear = yyyy + 1,
-      dayMonth = "11/16/",
+      dayMonth = "11/28/",
       conference_date = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
